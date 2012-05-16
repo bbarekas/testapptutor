7dcea7566f99eaccec0269c59c12169cb3805026
Bill Barekas <bbarekas@gmail.com>
2012-05-17 01:42:24 +0300

    After "Using the webapp Framework" step

--- main.py
+++ main.py
@@ -14,11 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello my new world!')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
-                              debug=True)
+application = webapp.WSGIApplication(
+    [('/', MainPage)],
+    debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
