e5bedc208fa7860c27b4d66cb6611cfc32496550
Bill Barekas <bbarekas@gmail.com>
2012-05-17 01:08:30 +0300

    First commit

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        self.response.out.write('Hello my world!')
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
                               debug=True)
